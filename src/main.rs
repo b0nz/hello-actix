8c80f32b4191b30f226d16289e1a6216d99729d3
b0nz <elliantosarono.p@gmail.com>
2020-05-14 23:31:42 +0700

    get todo_list

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,29 @@
+mod config;
 mod models;
+mod handlers;
+mod db;
 
-use crate::models::Status;
-use actix_web::{HttpServer, App, web, Responder};
+use actix_web::{HttpServer, App, web};
 use std::io;
-
-async fn status() -> impl Responder {
-    web::HttpResponse::Ok()
-        .json(Status { status: "ok".to_string()})
-}
+use dotenv::dotenv;
+use tokio_postgres::NoTls;
+use crate::handlers::*;
 
 #[actix_rt::main]
 async fn main() -> io::Result<()>{
-    HttpServer::new(|| {
+    dotenv().ok();
+
+    let config = crate::config::Config::from_env().unwrap();
+
+    let pool = config.pg.create_pool(NoTls).unwrap();
+
+    HttpServer::new(move || {
         App::new()
+            .data(pool.clone())
             .route("/", web::get().to(status))
+            .route("/todos{_:/?}", web::get().to(get_todos))
     })
-    .bind("127.0.0.1:8080")?
+    .bind(format!("{}:{}", config.server.host, config.server.port))?
     .run()
     .await
 }
